c9bf7bf59a570be3b579da23862464e2d04320d8
atraboulsi27 <a.traboulsi1@gmail.com>
2026-03-28 17:26:07 +0100

    uart send line

--- Core/Inc/uart.h
+++ Core/Inc/uart.h
@@ -4,5 +4,5 @@
 #include "stm32f4xx_hal.h"
 
 void uart_send(const UART_HandleTypeDef huart, const char *msg);
-
+void uart_send_line(const UART_HandleTypeDef huart, const char *msg);
 #endif
--- Core/Src/uart.c
+++ Core/Src/uart.c
@@ -5,3 +5,8 @@
 void uart_send(const UART_HandleTypeDef huart, const char *msg){
     HAL_UART_Transmit(&huart, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
 }
+
+void uart_send_line(const UART_HandleTypeDef huart, const char *msg){
+    uart_send(huart, msg);
+    uart_send(huart, "\r\n");
+}
